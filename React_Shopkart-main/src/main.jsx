9f5d0be4b9e1cc84d816978da741ae58d4054471
Nisarg <stopaaunga@gmail.com>
2026-08-24 18:22:35 +0530

    g

--- React_Shopkart-main/src/main.jsx
+++ React_Shopkart-main/src/main.jsx
@@ -5,8 +5,7 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App/>
+createRoot(document.getElementById("root")).render(
+  <StrictMode><App/></StrictMode>
+  
 )
